0ca235d61288cf3b99f58378e3ac619ef9a32c11
Saemi Park <saemipqrk7391@gmail.com>
2023-12-19 14:03:29 -0500

    study plan

---
+++ React-Redux/reactredux/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class AddNumberRoot extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Add Number Root</h1>
+			</div>
+		);
+	}
+}
+
+function App() {
+	return <div className="App">Hello World</div>;
+}
+
+export default App;
