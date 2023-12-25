ad37e876af96ab495c24e0d335a4533d13e74ff7
Saemi Park <saemipqrk7391@gmail.com>
2023-12-25 17:26:16 -0500

    making simple webpage

--- React/newver_2022/my-app/src/App.js
+++ React/newver_2022/my-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>
+					<a href="/">WEB</a>
+				</h1>
+			</header>
+			<nav>
+				<li>
+					<a href="/read/1">HTML</a>
+				</li>
+				<li>
+					<a href="/read/2">CSS</a>
+				</li>
+				<li>
+					<a href="/read31">JavaScript</a>
+				</li>
+			</nav>
+			<article>
+				<h2>Welcom</h2>
+				Hello, WEB
+			</article>
+		</div>
+	);
 }
 
 export default App;
